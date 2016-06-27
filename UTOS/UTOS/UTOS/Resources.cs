6d3166ceb17b4ac3bfddce9a6e3d245dc688f954
Alma Jensen <alma.jensen@landesk.com>
2016-06-27 15:01:41 -0600

    Added a resources class
    Added a resources class to store basic needed items.

---
+++ UTOS/UTOS/UTOS/Resources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTOS
+{
+    public class Resources
+    {
+        public const string GravatarBaseURL = "https://www.gravatar.com/avatar/";
+        public const string GravatarEnding = ".jpg";
+    }
+}
