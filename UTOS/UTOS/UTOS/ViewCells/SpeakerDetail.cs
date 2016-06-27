3e5705b31c236300aea7874d5d438549a45ad572
Alma Jensen <alma.jensen@landesk.com>
2016-06-27 14:43:31 -0600

    Added A Speaker Detail ViewCell
    Added a Speadker DetailViewCell

---
+++ UTOS/UTOS/UTOS/ViewCells/SpeakerDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UTOS.ViewCells
+{
+    public class SpeakerDetail : ViewCell
+    {
+        public SpeakerDetail()
+        {
+            MainLayout.Children.Add(Hero);
+            var innerLayout = new StackLayout()
+            {
+                Children =
+                {
+                    Name, TwitterHandle
+                }
+            };
+            MainLayout.Children.Add(innerLayout);
+            View = MainLayout;
+        }
+        Image Hero = new Image();
+        Label Name = new Label();
+        Label TwitterHandle = new Label();
+        StackLayout MainLayout = new StackLayout() {Orientation = StackOrientation.Horizontal};
+    }
+
+}
