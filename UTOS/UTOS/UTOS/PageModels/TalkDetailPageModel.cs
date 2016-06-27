a8531dbfbb4b6f495d52f9cfa0625683acc60d07
Alma Jensen <alma.jensen@landesk.com>
2016-06-27 14:51:15 -0600

    Added Speaker Detail into Talk Detail Page
    Added Speaker Detail into Talk Detail Page.

--- UTOS/UTOS/UTOS/PageModels/TalkDetailPageModel.cs
+++ UTOS/UTOS/UTOS/PageModels/TalkDetailPageModel.cs
@@ -2,6 +2,7 @@
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,12 @@ namespace UTOS.PageModels
     [ImplementPropertyChanged]
     public class TalkDetailPageModel : FreshBasePageModel
     {
-        public Talk SeletectedTalk {get;set;}
+        public Talk SeletectedTalk { get; set; } = new Talk();
+        public ObservableCollection<Speaker> Speakers { get; set; } = new ObservableCollection<Speaker>();
         public override void Init(object initData)
         {
             base.Init(initData);
-
+            
             SeletectedTalk = initData as Talk;
             if (SeletectedTalk == null)
                 SeletectedTalk = new Talk();
--- UTOS/UTOS/UTOS/Pages/TalkDetailPage.cs
+++ UTOS/UTOS/UTOS/Pages/TalkDetailPage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using UTOS.PageModels;
+using UTOS.ViewCells;
 using Xamarin.Forms;
 
 namespace UTOS.Pages
@@ -14,13 +15,17 @@ namespace UTOS.Pages
         Label TalkTitle = new Label();
         Label Track = new Label();
         Label Description = new Label();
+        ListView Speaker = new ListView(ListViewCachingStrategy.RecycleElement);
         public TalkDetailPage()
         {
+            Speaker.HasUnevenRows = true;
+            Speaker.ItemTemplate = new DataTemplate(typeof(SpeakerDetail));
+            Speaker.SetBinding(ListView.ItemsSourceProperty, nameof(TalkDetailPageModel.Speakers));
             Time.SetBinding(Label.TextProperty, "SeletectedTalk.ts");
             Content = new StackLayout
             {
                 Children = {
-                    Time, TalkTitle, Track, Description
+                    Time, TalkTitle, Speaker, Track, Description
                 }
             };
         }
