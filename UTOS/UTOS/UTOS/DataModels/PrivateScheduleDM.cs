4295734091a343d41947c818753b936a58e3d198
AlmaJensen <jazzdevstudio@gmail.com>
2016-06-28 18:53:27 -0600

    Little more work for storage service
    Added a little more work for storage service.

---
+++ UTOS/UTOS/UTOS/DataModels/PrivateScheduleDM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTOS.DataModels
+{
+    public class PrivateScheduleDM
+    {
+        public string UTOSScheduleID { get; set; }
+        public List<SessionDM> PlannedSessions { get; set; }
+    }
+}
